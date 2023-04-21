91a4adf3d886eba9770bb2833ab8da46afefceb1
Yuan Tang <terrytangyuan@gmail.com>
2023-04-21 13:51:01 -0400

    fix: Unset resourceVersion for LIST calls
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- workflow/controller/controller.go
+++ workflow/controller/controller.go
@@ -805,6 +805,7 @@ func (wfc *WorkflowController) tweakListOptions(options *metav1.ListOptions) {
 	labelSelector := labels.NewSelector().
 		Add(util.InstanceIDRequirement(wfc.Config.InstanceID))
 	options.LabelSelector = labelSelector.String()
+	options.ResourceVersion = ""
 }
 
 func getWfPriority(obj interface{}) (int32, time.Time) {
